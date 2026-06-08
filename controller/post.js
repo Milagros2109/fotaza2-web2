e50e2823dddbab1a7987ec36ae0d7c644b794f41
Alfaro <alfaromilagrosg@gmail.com>
2026-06-08 00:27:34 -0300

    Implemento buscador de publicaciones

--- controller/post.js
+++ controller/post.js
@@ -1,13 +1,27 @@
+import { Op } from 'sequelize';
 import { Post } from '../models/Post.js';
 
 export const postList = async (req, res) => {
+  const search = req.query.search || '';
+
+  const where = search
+    ? {
+        [Op.or]: [
+          { title: { [Op.iLike]: `%${search}%` } },
+          { description: { [Op.iLike]: `%${search}%` } }
+        ]
+      }
+    : {};
+
   const posts = await Post.findAll({
+    where,
     order: [['createdAt', 'DESC']]
   });
 
   res.render('posts', {
     pagina: 'Publicaciones',
-    posts
+    posts,
+    search
   });
 };
 
@@ -29,7 +43,6 @@ export const createPost = async (req, res) => {
 
   } catch (error) {
     console.error(error);
-
     res.render('post/crear');
   }
 };
